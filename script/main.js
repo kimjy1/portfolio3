b183713d7c2e13808423e68303b529e04f8d70df
kimjy1 <93234891+kimjy1@users.noreply.github.com>
2022-01-07 16:00:11 +0900

    main page coding

--- script/main.js
+++ script/main.js
@@ -14,3 +14,14 @@ $(window).scroll(function () {
     }
     lastScrollTop = scrollTop;
 });
+
+var i = 0;
+function slide() {
+    i++;
+    if (i > $('.slide li:last').index()) {
+        i = 0;
+    }
+    $('.slide li').eq(i).stop().fadeIn('slow');
+    $('.slide li').eq(i - 1).stop().fadeOut('slow');
+}
+setInterval(slide, 10000);
